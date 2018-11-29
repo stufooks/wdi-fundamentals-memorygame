7c353fa271f991a2dbd123801db550934965becd
Stuart Fooks <Stuart@MacBook-Pro-4.domain>
2018-11-29 09:03:42 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,21 +2,27 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-console.log("User just flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User just flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function (){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+	} else {
+		alert("Sorry try again!");
 	}
-	else {
-		alert("Sorry try again!")
-	}
-}
+};
+
+
+var flipCard = function (cardId){
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
+
+
+flipCard(0);
+flipCard(2);
+
+
+
